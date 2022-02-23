669c45458bcb2e2dd5f70235fc9cb8e1b4d7616d
thiswillbeyourgithub <github@32mail.33mail.comm>
2022-02-23 18:25:34 +0100

    major version : untested algo, added settings panel

--- README.md
+++ README.md
@@ -5,46 +5,32 @@
 * I created this repository before even receiving my pine time and despite a very busy schedule to make sure no one else starts a similar project and end up duplicating efforts for nothing :)
 * If you're interested or have any kind of things to say about this, **please** open an issue and tell me all about it :)
 * Status as of February 23rd:
-    * Currently building a sleep logger app, this will help run code on the simulator
+    * First version finished, the waking up algorithm is not at all tested
     * **Instructions**: (with a forked wasp-os that exposes accel data : [link](https://github.com/thiswillbeyourgithub/wasp-os))
         * get the latest python file : ZzzTracker.py
         * compile it : `./micropython/mpy-cross/mpy-cross -mno-unicode -march=armv7m ZzzTracker.py`
         * send compiled : `./tools/wasptool --verbose --upload ZzzTracker.mpy --as apps/ZzzTracker.mpy --binary`
         * register compiled : `./tools/wasptool --verbose --eval "wasp.system.register('apps.ZzzTracker.ZzzTrackerApp')`
         * run it!
-        * get back the data using `wasptool --pull`
+        * if you want, get back the data using `wasptool --pull`
         * take a look at it using pandas, for example using : ` df = pd.read_csv("./first.night.csv", names=["time", "x_avg", "y_avg", "z_avg", "angl_avg", "battery"])` (name and number of columns might change)
 
-## Roadmap / Currently planned features:
-**First step**
-* ~~focus on logging your sleep to accumulate data and share it on github~~DONE. The more data you have the easier it will be to calibrate the algorithm.
-
+## TODO
 **sleep tracking**
-* tracks sleep using wrist motion data and occasional heart rate monitoring
-    * each night is recorded in a file that can be easily sent back to the phone
-* rudimentary display of sleep graph on the device itself, with a quality score if I can find a good metric
-* try to roughly infer the sleep stage *on the device itself*
+* try to roughly infer the sleep stage directly on the device?
     * if you actually use the watch during the night, make sure to count it as wakefulness
 
-**alarm clock**
-* setting up an alarm should suggest the most appropriate sleep duration like what [sleepyti.me](http://sleepyti.me) does, so either sleep duration or by wake up time
-* try to optimize the wake up time based on inferred sleep stage
-
 **settings panel**
-* to specify how early the watch can wake you
-* to specify a battery threshold under which it should not keep tracking sleep, to make sure you don't drain the battery and end up missing the alarm clock
+* specify a battery threshold under which it should not keep tracking sleep, to make sure you don't drain the battery and end up missing the alarm clock
 
 **misc**
-* turn off the Bluetooth connection when no phone is connected
-* turn off the screen during the night
-* make sure to not use more than X% of the battery in all cases
-* make sure to turn off if sleep lasts more than 12h (in which case the user forgot to disable it)
+* turn off the Bluetooth connection when no phone is connected?
+* confirmation screen when disabling sleep tracking
 * ability to send in real time to Bluetooth device the current sleep stage you're probably in. For use in Targeted Memory Reactivation.
-* hardcode limits to avoid issues if heart rate is suddenly found to be through the roof or something
-* maybe the least cpu intensive way to compute optimal wake up time would be to compute least square difference with sinusoidal of varying periods and phases.
+* find a way to remove outliers of stored values
 
 **Features that I'm note sure yet**
-* should the watch ask you after waking up to rate your sleep on a simple scale?
+* should the watch ask you after waking up to rate your freshness at wake?
 
 ## Related links:
 * article with detailed implementation : https://www.nature.com/articles/s41598-018-31266-z
--- ZzzTracker.py
+++ ZzzTracker.py
@@ -15,51 +15,35 @@ Current state:
 Trying to log my sleep data for a few days prior to working on the algorithm
 """
 
-from os import stat
+import time
 from wasp import watch, system, EventMask, gc
 
 from watch import rtc, battery, accel
-from widgets import Clock, BatteryMeter, Button
+from widgets import Clock, BatteryMeter, Button, Spinner, Checkbox
 from shell import mkdir, cd
 from fonts import sans18
 
 from math import atan, pow, degrees, sqrt, sin, pi
-from micropython import const
 from array import array
 
 _FONT = sans18
-
-# to activate DEBUG MODE : comment the next paragraph and uncomment DEBUG BLOCK
-
-_POLLFREQ = const(5)  # get accelerometer data every X seconds, they will
-# be averaged
-_WIN_L = const(300)  # number of seconds between storing average values to file
-# written every X points
-
-_WU_ON = const(1)  # const(1) to activate wake up alarm, const(0) to disable
-_WU_LAT = const(27000)  # maximum seconds of sleep before waking you up,
-# default 27000 = 7h30
-_WU_ANT_ON = const(0)  # set to 1 to activate waking you up at optimal time
-# based on accelerometer data, at the earliest at _WU_LAT - _WU_ANTICIP
-_WU_ANTICIP = const(1800)  # default 1800 = 30 minutes
-
-
-# DEBUG BLOCK:
-#_POLLFREQ = const(1)
-#_WIN_L = const(5)
-#_WU_ON = const(1)
-#_WU_LAT = const(30)
-#_WU_ANT_ON = const(1)
-#_WU_ANTICIP = const(5)
-
 
 class ZzzTrackerApp():
     NAME = 'ZzzTrck'
 
     def __init__(self):
+        self._wakeup_enabled = 1
+        self._wakeup_ant_enabled = 1  # activate waking you up at optimal time  based on accelerometer data, at the earliest at _WU_LAT - _WU_ANTICIP
+        self._freq = 5  # get accelerometer data every X seconds, they will be averaged
+        self._store_freq = 300  # number of seconds between storing average values to file written every X points
+        self._wakeup_ant_latitude = 1800  # defaults 1800 = 30m
+        self._spinval_H = 7  # default wake up time
+        self._spinval_M = 30
+        self._debug = False
         self._tracking = False  # False = not tracking, True = currently tracking
         self._earlier = 0
-        self._page = "START" # can be START / TRACKING / RINGING / WAITING_EARLY_WU
+        self._page = "START" # can be START / TRACKING / RINGING / WAITING_EARLY_WU / SETTINGS
+
         try:
             mkdir("logs/")
         except:  # folder already exists
@@ -81,6 +65,7 @@ class ZzzTrackerApp():
 
     def touch(self, event):
         """either start trackign or disable it, draw the screen in all cases"""
+        no_full_draw = False
         if self._page == "START":
             if self.btn_on.touch(event):
                 self._tracking = True
@@ -94,16 +79,32 @@ class ZzzTrackerApp():
                 self.filep = "logs/sleep/" + str(self._offset) + ".csv"
                 self._add_accel_alar()
 
-                # alarm in _WU_LAT seconds after tracking started to wake you up
-                if _WU_ON:
-                    self._WU_t = self._offset + _WU_LAT
+                if self._debug:
+                    self._freq = 1
+                    self._store_freq = 5
+                    self._wakeup_ant_latitude = 5
+
+                # alarm in self._SL_L seconds after tracking started to wake you up
+                self._SL_L = self._spinval_H*60*60 + self._spinval_M*60
+                if self._wakeup_enabled:
+                    now = rtc.get_localtime()
+                    yyyy = now[0]
+                    mm = now[1]
+                    dd = now[2]
+                    HH = self._spinval_H
+                    MM = self._spinval_M
+                    if HH < now[3] or (HH == now[3] and MM <= now[4]):
+                        dd += 1
+                    self._WU_t = time.mktime((yyyy, mm, dd, HH, MM, 0, 0, 0, 0))
                     system.set_alarm(self._WU_t, self._listen_to_ticks)
 
-                    # alarm in _WU_ANTICIP less seconds to compute best wake up time
-                    if _WU_ANT_ON:
-                        self._WU_a = self._WU_t - _WU_ANTICIP
+                    # alarm in self._wakeup_ant_latitude less seconds to compute best wake up time
+                    if self._wakeup_ant_enabled:
+                        self._WU_a = self._WU_t - self._wakeup_ant_latitude
                         system.set_alarm(self._WU_a, self._compute_best_WU)
                 self._page = "TRACKING"
+            elif self.btn_set.touch(event):
+                self._page = "SETTINGS"
         elif self._page == "TRACKING":
             if self.btn_off.touch(event):
                 self._disable_tracking()
@@ -112,7 +113,47 @@ class ZzzTrackerApp():
             if self.btn_al.touch(event):
                 self._disable_tracking()
                 self._page = "START"
-        self._draw()
+        elif self._page == "SETTINGS":
+            no_full_draw = True
+            disable_both = False
+            if self.check_al.touch(event):
+                if self._wakeup_enabled == 1:
+                    self._wakeup_enabled = 0
+                    disable_both = True
+                else:
+                    self._wakeup_enabled = 1
+                self.check_al.state = self._wakeup_enabled
+                self.check_al.update()
+            if self.check_anti.touch(event) or disable_both:
+                if self._wakeup_ant_enabled == 1 or disable_both:
+                    self._wakeup_ant_enabled = 0
+                    self.check_anti.state = self._wakeup_ant_enabled
+                    self._check_anti = None
+                    self._draw()
+                elif self._wakeup_enabled == 1:
+                    self._wakeup_ant_enabled = 1
+                    self.check_anti.state = self._wakeup_ant_enabled
+                    self.check_anti.update()
+            elif self.hours.touch(event):
+                self._spinval_H = self.hours.value
+                self.hours.update()
+            elif self.min.touch(event):
+                self._spinval_M = self.min.value
+                self.min.update()
+            elif self.check_debug.touch(event):
+                if self._debug:
+                    self._debug = False
+                else:
+                    self._debug = True
+                self.check_debug.update()
+            elif self.btn_set_end.touch(event):
+                self._page = "START"
+                self._draw()
+            else:
+                self._draw()
+
+        if no_full_draw is False:
+            self._draw()
 
     def _disable_tracking(self):
         """called by touching "STOP TRACKING" or when computing best alarm time
@@ -120,21 +161,21 @@ class ZzzTrackerApp():
         disables tracking features and alarms"""
         self._tracking = False
         system.cancel_alarm(self.next_al, self._trackOnce)
-        if _WU_ON:
+        if self._wakeup_enabled:
             system.cancel_alarm(self._WU_t, self._listen_to_ticks)
-            if _WU_ANT_ON:
+            if self._wakeup_ant_enabled:
                 system.cancel_alarm(self._WU_a, self._compute_best_WU)
         self._periodicSave()
 
     def _add_accel_alar(self):
-        """set an alarm, due in _POLLFREQ minutes, to log the accelerometer data
+        """set an alarm, due in self._freq minutes, to log the accelerometer data
         once"""
-        self.next_al = watch.rtc.time() + _POLLFREQ
+        self.next_al = watch.rtc.time() + self._freq
         system.set_alarm(self.next_al, self._trackOnce)
 
     def _trackOnce(self):
-        """get one data point of accelerometer every _POLLFREQ seconds and
-        they are then averaged and stored every _WIN_L seconds"""
+        """get one data point of accelerometer every self._freq seconds and
+        they are then averaged and stored every self._store_freq seconds"""
         if self._tracking:
             [self._buff.append(x) for x in accel.read_xyz()]
             self._data_point_nb += 1
@@ -143,7 +184,7 @@ class ZzzTrackerApp():
 
     def _periodicSave(self):
         """save data after averageing over a window to file"""
-        if self._data_point_nb - self._last_checkpoint >= _WIN_L / _POLLFREQ:
+        if self._data_point_nb - self._last_checkpoint >= self._store_freq / self._freq:
             x_avg = sum([self._buff[i] for i in range(0, len(self._buff), 3)]) / (self._data_point_nb - self._last_checkpoint)
             y_avg = sum([self._buff[i] for i in range(1, len(self._buff), 3)]) / (self._data_point_nb - self._last_checkpoint)
             z_avg = sum([self._buff[i] for i in range(2, len(self._buff), 3)]) / (self._data_point_nb - self._last_checkpoint)
@@ -182,23 +223,49 @@ class ZzzTrackerApp():
         elif self._page in ["TRACKING", "WAITING_EARLY_WU"]:
             draw.string('Started at ' + ":".join([str(x) for x in watch.time.localtime(self._offset)[3:5]]), 0, 70)
             draw.string("data points:" + str(self._data_point_nb), 0, 90)
-            if _WU_ON:
-                if _WU_ANT_ON:
+            if self._wakeup_enabled:
+                if self._wakeup_ant_enabled:
                     word = " before "
                 else:
                     word = " at "
-                draw.string("Wake up" + word + ":".join([str(x) for x in watch.time.localtime(self._offset + _WU_LAT)[3:5]]), 0, 130)
+                draw.string("Wake up" + word + ":".join([str(x) for x in watch.time.localtime(self._offset + self._SL_L)[3:5]]), 0, 130)
             self.btn_off = Button(x=0, y=170, w=240, h=69, label="Stop tracking")
             self.btn_off.draw()
         elif self._page == "START":
             draw.string('Sleep tracker' , 0, 70)
-            self.btn_on = Button(x=0, y=170, w=240, h=69, label="Start tracking")
+            self.btn_on = Button(x=0, y=170, w=200, h=40, label="Start tracking")
             self.btn_on.draw()
+            self.btn_set = Button(x=201, y=170, w=38, h=40, label="S")
+            self.btn_set.draw()
+        elif self._page == "SETTINGS":
+            draw.string("Settings", 0, 0)
+            self.btn_set_end = Button(x=201, y=0, w=38, h=40, label="X")
+            self.btn_set_end.draw()
 
-        self.cl = Clock(True)
-        self.cl.draw()
-        bat = BatteryMeter()
-        bat.draw()
+            self.hours = Spinner(0, 5, 0, 23, 2)
+            self.hours.value = self._spinval_H
+            self.hours.draw()
+            self.min = Spinner(60, 5, 0, 59, 2)
+            self.min.value = self._spinval_M
+            self.min.draw()
+
+            self.check_debug = Checkbox(x=0, y=120, label="Debug?")
+            self.check_debug.state = self._debug
+            self.check_debug.draw()
+            self.check_al = Checkbox(x=0, y=160, label="Alarm?")
+            self.check_al.state = self._wakeup_enabled
+            self.check_al.draw()
+            if self.check_al.state == 1:
+                self.check_anti = Checkbox(x=0, y=200, label="Anticipate?")
+                self.check_anti.state = self._wakeup_ant_enabled
+                self.check_anti.draw()
+
+
+        if self._page != "SETTINGS":
+            self.cl = Clock(True)
+            self.cl.draw()
+            bat = BatteryMeter()
+            bat.draw()
 
     def _compute_best_WU(self):
         """computes best wake up time from sleep data"""
@@ -228,8 +295,8 @@ class ZzzTrackerApp():
         omega = 2 * pi / 324000  # 90 minutes, average sleep cycle duration
         for cnt, offset in enumerate(offsets):  # least square regression
             fits.append(
-                    sum([sin(omega * t * _WIN_L + offset) * data[t] for t in range(len(data))])
-                    -sum([(sin(omega * t * _WIN_L + offset) - data[t])**2 for t in range(len(data))])
+                    sum([sin(omega * t * self._store_freq + offset) * data[t] for t in range(len(data))])
+                    -sum([(sin(omega * t * self._store_freq + offset) - data[t])**2 for t in range(len(data))])
                     )
             if fits[-1] == min(fits):
                 best_offset = offsets[cnt]
@@ -237,7 +304,7 @@ class ZzzTrackerApp():
 
         # finding how early to wake up:
         max_sin = 0
-        for t in range(self._WU_t, self._WU_t - _WU_ANTICIP, -300):  # counting backwards from original wake up time, steps of 5 minutes
+        for t in range(self._WU_t, self._WU_t - self._wakeup_ant_latitude, -300):  # counting backwards from original wake up time, steps of 5 minutes
             s = sin(omega * t + best_offset)
             if s > max_sin:
                 max_sin = s
