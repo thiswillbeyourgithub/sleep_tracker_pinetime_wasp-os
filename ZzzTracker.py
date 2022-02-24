374cc649a8145b967fe564d37981f4ff26af7c7f
thiswillbeyourgithub <github@32mail.33mail.comm>
2022-02-24 11:34:38 +0100

    smaller STOP button

--- ZzzTracker.py
+++ ZzzTracker.py
@@ -218,7 +218,7 @@ class ZzzTrackerApp():
             else:
                 msg = "WAKE UP"
             draw.string(msg, 0, 70)
-            self.btn_al = Button(x=0, y=170, w=240, h=69, label="STOP")
+            self.btn_al = Button(x=0, y=170, w=240, h=40, label="STOP")
             self.btn_al.draw()
         elif self._page in ["TRACKING", "WAITING_EARLY_WU"]:
             draw.string('Started at ' + ":".join([str(x) for x in watch.time.localtime(self._offset)[3:5]]), 0, 70)
