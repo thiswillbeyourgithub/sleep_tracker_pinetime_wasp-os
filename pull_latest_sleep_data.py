31ce2ddc24f5fd59821e743090bbbac1bf6c0fee
thiswillbeyourgithub <github@32mail.33mail.comm>
2022-02-27 15:26:48 +0100

    new: ability to download all files + skip already present

--- pull_latest_sleep_data.py
+++ pull_latest_sleep_data.py
@@ -6,16 +6,29 @@ import subprocess
 import shlex
 import re
 
-ls_cmd = './tools/wasptool --verbose --eval \'from shell import ls ; ls(\"logs/sleep\")\''
+mode = "all"  # download "all" files or only "latest"
+assert mode in ["all", "latest"]
+
+ls_cmd = './tools/wasptool --verbose --eval \'from shell import ls ; ls(\"/flash/logs/sleep/\")\''
 ls_cmd = shlex.split(ls_cmd)  # properly split args
 print("Listing remote files...")
 out = subprocess.check_output(ls_cmd).decode()
 files = re.findall(r"\d*\.csv", out)
 print(f"Found files {', '.join(files)}")
-latest = files[-1]
-print(f"Most recent file is: {latest}")
 
-pull_cmd = f'./tools/wasptool --verbose --pull logs/sleep/{latest}'
-os.system(pull_cmd)
+if mode == "latest":
+    to_dl = files[-1]
 
-print(f"Succesfully downloaded file to 'logs/sleep/{latest}'")
+elif mode == "all":
+    to_dl = files
+
+print("\n\n")
+for fi in to_dl:
+    if os.path.exists(f"./logs/sleep/{fi}"):
+        print(f"Skipping file {fi}: already exists")
+    else:
+        print(f"Downloading file '{fi}'")
+        pull_cmd = f'./tools/wasptool --verbose --pull logs/sleep/{fi}'
+        os.system(pull_cmd)
+        print(f"Succesfully downloaded to './logs/sleep/{fi}'")
+    print("\n\n")
