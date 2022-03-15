706144b30cb19f2c353df6c7991d9a2f49b523e1
thiswillbeyourgithub <github@32mail.33mail.comm>
2022-03-15 13:01:10 +0100

    vibrate a tiny bit when using the spinner

--- SleepTk.py
+++ SleepTk.py
@@ -126,6 +126,7 @@ class SleepTkApp():
                 self._spin_H.update()
                 self._spinval_M = self._spin_M.value
                 self._spin_M.update()
+                wasp.watch.vibrator.pulse(25, 15)
                 if self._alarm_state:
                     draw = wasp.watch.drawable
                     draw.set_font(_FONT)
