eeb9eddb701f1a35f0c3932d9a6daec71eee4b09
thiswillbeyourgithub <github@32mail.33mail.com>
2022-04-22 18:22:13 +0200

    minor

--- SleepTk.py
+++ SleepTk.py
@@ -257,7 +257,7 @@ class SleepTkApp():
                 draw.string("(Currently tracking HR)", 0, 170)
             if self._state_HR_tracking:
                 draw.string("HR:{}".format(self._last_HR_printed), 160, 170)
-            self.btn_off = widgets.Button(x=0, y=200, w=240, h=40, label="Stop tracking")
+            self.btn_off = widgets.Button(x=0, y=200, w=240, h=40, label="Stop")
             self.btn_off.draw()
             draw.reset()
             self._draw_duration(draw)
