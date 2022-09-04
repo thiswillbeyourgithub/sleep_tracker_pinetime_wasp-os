ddf568d74d04cdb3613a4baba218719a70653ecd
olivier-cornelis <klduycea@mailer.me>
2022-09-04 16:39:52 +0200

    new: record sleep data by default

--- SleepTk.py
+++ SleepTk.py
@@ -100,8 +100,8 @@ class SleepTkApp():
 
         # default button state
         self._state_alarm = _ON
-        self._state_body_tracking = _OFF
-        self._state_HR_tracking = _OFF
+        self._state_body_tracking = _ON
+        self._state_HR_tracking = _ON
         self._state_gradual_wake = _ON
         self._state_spinval_H = _OFF
         self._state_spinval_M = _OFF
