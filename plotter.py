9f555a327c05485c8e7c31aac2ff89c56c31b6c1
olivier-cornelis <klduycea@mailer.me>
2022-09-06 17:52:16 +0200

    new: auto plotting of recordings

--- plotter.py
+++ plotter.py
@@ -3,6 +3,7 @@ import pandas as pd
 from tqdm import tqdm
 from numpy import arctan, pi
 from datetime import datetime
+import matplotlib.pyplot as plt
 
 local_dir = Path("./remote_files/logs/sleep/")
 
@@ -12,7 +13,7 @@ files = [f for f in local_dir.iterdir()]
 assert len(files) > 0, "No files found."
 print(f"{len(files)} files found.")
 
-df_list = []
+recordings = {}
 for file in tqdm(files, desc="Loading files"):
     df = pd.read_csv(file)
 
@@ -23,10 +24,20 @@ for file in tqdm(files, desc="Loading files"):
     df["date"] = [datetime.utcfromtimestamp(unix) for unix in df["UNIX_time"].tolist()]
     df.set_index("Timestamp")
 
+    recording_date = str(datetime.fromtimestamp(offset))
+
     if len(df.index.tolist()) == 0:
         tqdm.write(f"No data in df '{file}'")
     else:
-        df_list.append(df)
+        recordings[recording_date] = df
+        plt.plot(df["date"], df["arm_angle_approximation"])
+        plt.title(recording_date)
+        for ind in df.index:
+            if df.loc[ind, "Touched"] == 1:
+                plt.axvline(x=df.loc[ind, "date"])
+        plt.show()
 
-print("Loaded files as dataframe as element of list 'df_list'. Openning console.")
+
+df = recordings[recordings.keys()[-1]]
+print("Loaded files as dataframe as values of dict 'recordings'. Openning console.")
 import code ;code.interact(local=locals())
