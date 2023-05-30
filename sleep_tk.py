186cb531640406ada1582aa723b82b1699ff7e0b
thiswillbeyourgithub <github@32mail.33mail.com>
2023-05-30 17:24:14 +0200

    readd code that try to avoid vibrating when the alarm was turned of

--- sleep_tk.py
+++ sleep_tk.py
@@ -727,6 +727,9 @@ class SleepTkApp():
     def _activate_ticks_to_ring(self):
         """listen to ticks every second, telling the watch to vibrate and
         completely wake the user up"""
+        if not hasattr(self, "_WU_t") or self._WU_t is None:
+            # alarm was already started and stopped
+            return
         wasp.system.wake()
         wasp.system.switch(self)
         self._page = _RINGING
@@ -743,6 +746,9 @@ class SleepTkApp():
 
     def _start_natural_wake(self):
         """do a tiny vibration every 30s until the user wakes up"""
+        if not hasattr(self, "_WU_t") or self._WU_t is None:
+            # alarm was already started and stopped
+            return
         wasp.system.wake()
         wasp.system.switch(self)
         wasp.gc.collect()
